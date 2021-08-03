faabe21171de9125e6ad9b5c0fe22bded6092c9f
hughjph <hughjohnson2018@outlook.com>
2021-08-03 18:46:59 +0100

    fix compass menu identifier

--- src/main/java/gg/eris/lobby/menu/CompassMenu.java
+++ src/main/java/gg/eris/lobby/menu/CompassMenu.java
@@ -3,6 +3,7 @@ package gg.eris.lobby.menu;
 import gg.eris.commons.bukkit.menu.Menu;
 import gg.eris.commons.bukkit.menu.MenuItem;
 import gg.eris.commons.bukkit.menu.MenuViewer;
+import gg.eris.commons.core.identifier.Identifier;
 import gg.eris.lobby.menu.item.GoldenApple;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
@@ -12,7 +13,7 @@ public class CompassMenu extends Menu {
     private static final String title = "Game Selector";
 
     public CompassMenu(JavaPlugin owningPlugin) {
-        super(owningPlugin, "gameSelector", 3);
+        super(owningPlugin, Identifier.of("menu", "game selector"), 3);
         setFillItem(new ItemStack(Material.STAINED_GLASS_PANE));
         MenuItem goldenApple = new GoldenApple();
         addItem(13, goldenApple);
