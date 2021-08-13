34a2e6d00351baf5525746441e90a1a74a467108
alfiejfs <alfie@alfiesmith.net>
2021-08-13 01:14:25 +0100

    lobby

--- src/main/java/gg/eris/lobby/menu/CompassMenu.java
+++ src/main/java/gg/eris/lobby/menu/CompassMenu.java
@@ -3,15 +3,16 @@ package gg.eris.lobby.menu;
 import gg.eris.commons.bukkit.menu.Menu;
 import gg.eris.commons.bukkit.menu.MenuViewer;
 import gg.eris.commons.core.identifier.Identifier;
+import gg.eris.lobby.ErisLobby;
 import gg.eris.lobby.menu.item.UhcMenuItem;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CompassMenu extends Menu {
 
-  public CompassMenu(JavaPlugin owningPlugin) {
-    super(owningPlugin, Identifier.of("compass", "game_selector"), 3);
+  public CompassMenu(ErisLobby plugin) {
+    super(plugin, Identifier.of("compass", "game_selector"), 3);
     setFillItem(Menu.LIGHT_FILLER);
-    addItem(13, new UhcMenuItem());
+    addItem(13, new UhcMenuItem(plugin));
   }
 
   @Override
